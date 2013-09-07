f84f10c43e9d9771d85017122b00ddab2c555706
Diego Ortiz-Monasterio <dortizmonasterio@gmail.com>
2013-09-07 15:34:43 -0700

    Precompile assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,6 +3,7 @@ Vlm::Application.configure do
 
   # Code is not reloaded between requests.
   config.cache_classes = true
+  config.assets.compile = true
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both thread web servers
@@ -25,9 +26,6 @@ Vlm::Application.configure do
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
-
-  # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
 
   # Generate digests for assets URLs.
   config.assets.digest = true
